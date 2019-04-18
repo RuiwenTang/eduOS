892fc8bd688a6e9128bf239358c4f53067ac8618
Tang Ruiwen <wow.2007.t@163.com>
2019-04-19 01:40:32 +0800

    cpp demo

--- apps/filemanager/main.cpp
+++ apps/filemanager/main.cpp
@@ -4,6 +4,25 @@
 
 typedef boost::circular_buffer<int> circular_buffer;
 
+class BaseClass {
+public:
+    virtual ~BaseClass() {}
+
+    virtual void hello() = 0;
+};
+
+
+class SubClass: public BaseClass {
+public:
+    SubClass(): name("sub class") {}
+    virtual ~SubClass() {}
+    virtual void hello() override {
+        printf("name = %s\n", this->name.c_str());
+    }
+private:
+    std::string name;
+};
+
 int main(int argc, char const *argv[])
 {
     
@@ -12,5 +31,10 @@ int main(int argc, char const *argv[])
     printf("1\n");
     cb.push_back(1);
     printf("cb.size = %d\n", cb.size());
+
+    BaseClass* b = new SubClass();
+    b->hello();
+    delete b;
+
     return 0;
 }
